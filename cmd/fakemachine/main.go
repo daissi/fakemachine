e9dd9b6ee0eabe6b37710d1f96e13f891de2ace5
Dylan Aïssi <dylan.aissi@collabora.com>
2025-04-01 12:37:19 +0200

    print debug

--- cmd/fakemachine/main.go
+++ cmd/fakemachine/main.go
@@ -34,6 +34,24 @@ var parser = flags.NewParser(&options, flags.Default)
 func GetDeterminedVersion(version string) string {
 	DeterminedVersion := "unknown"
 
+	info, ok := debug.ReadBuildInfo();
+	if !ok {
+		fmt.Printf("No BuildInfo\n");
+	} else {
+		fmt.Printf("=== BuildInfo ===\n")
+		fmt.Printf("Main Path: %v\n", info.Main.Path);
+		fmt.Printf("Main Version: %v\n", info.Main.Version);
+		fmt.Printf("Main Sum: %v\n", info.Main.Sum);
+		for _, d := range info.Deps {
+			fmt.Printf("Dep: %v\n", d);
+		}
+		for _, s := range info.Settings {
+			fmt.Printf("Setting: %v\n", s);
+		}
+		fmt.Printf("======\n")
+	}
+	fmt.Printf("Version symbol: %v\n", Version);
+
 	// Use the injected Version from build system if any.
 	// Otherwise try to determine the best version string from debug info.
 	if len(version) > 0 {
